3928cf5d8855a12b4693f24469926336dd2ef5d2
deybyr647 <deybyr647@gmail.com>
2023-04-08 13:43:38 -0400

    API Route can now work with any amount of code given & explanation level can be chosen

--- pages/api/analyze.ts
+++ pages/api/analyze.ts
@@ -6,10 +6,24 @@ const openaiKey = process.env.OPENAI_KEY;
 import openai from "@/components/openai-config";
 
 const analyzer = async (req: NextApiRequest, res: NextApiResponse) => {
+    const explanationLevel = {
+        standard: `“””Here’s what the above code is doing, explained in a concise way`,
+        easy: `"""Explain the above code at a 5th grade level`,
+    }
+
+    const {code, explanation} = req.query;
+
+    let davinciPrompt: string;
+
+    if (explanation === "easy") {
+        davinciPrompt = code + explanationLevel.easy;
+    } else {
+        davinciPrompt = code + explanationLevel.standard;
+    }
 
     const options = {
         model: "text-davinci-003",
-        prompt: "class Log:\n    def __init__(self, path):\n        dirname = os.path.dirname(path)\n        os.makedirs(dirname, exist_ok=True)\n        f = open(path, \"a+\")\n\n        # Check that the file is newline-terminated\n        size = os.path.getsize(path)\n        if size > 0:\n            f.seek(size - 1)\n            end = f.read(1)\n            if end != \"\\n\":\n                f.write(\"\\n\")\n        self.f = f\n        self.path = path\n\n    def log(self, event):\n        event[\"_event_id\"] = str(uuid.uuid4())\n        json.dump(event, self.f)\n        self.f.write(\"\\n\")\n\n    def state(self):\n        state = {\"complete\": set(), \"last\": None}\n        for line in open(self.path):\n            event = json.loads(line)\n            if event[\"type\"] == \"submit\" and event[\"success\"]:\n                state[\"complete\"].add(event[\"id\"])\n                state[\"last\"] = event\n        return state\n\n\"\"\"\nHere's what the above class is doing, explained in a concise way:\n1.",
+        prompt: davinciPrompt,
         temperature: 0,
         max_tokens: 150,
         top_p: 1.0,
@@ -18,11 +32,10 @@ const analyzer = async (req: NextApiRequest, res: NextApiResponse) => {
         stop: ["\"\"\""],
     }
 
-
     const response = await openai.createCompletion(options);
     const {data} = response;
 
-    res.status(200).json({ ...data });
+    res.status(200).json({ ...data, ...req.query });
 }
 
 export default analyzer;
