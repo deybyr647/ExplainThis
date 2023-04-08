f1e72e23d648d7cf9b35cf36223c84479108619b
deybyr647 <deybyr647@gmail.com>
2023-04-08 13:45:23 -0400

    minor edits

--- pages/api/analyze.ts
+++ pages/api/analyze.ts
@@ -1,8 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-
-const openaiKey = process.env.OPENAI_KEY;
-
 import openai from "@/components/openai-config";
 
 const analyzer = async (req: NextApiRequest, res: NextApiResponse) => {
